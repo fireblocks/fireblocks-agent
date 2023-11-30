803881944f30120ca7f9107342feda8f7c0e41bb
Ofir Dagan <odagan@fireblocks.com>
2023-11-30 18:02:54 +0200

    feat(env): add support for dev env

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
-export const MOBILE_GATEWAY_URL = 'https://mobile-api.fireblocks.io';
+require('dotenv').config({
+  path: `${__dirname}/../.env.${process.env.NODE_ENV}`,
+});
+
+export const MOBILE_GATEWAY_URL = process.env.MOBILE_GATEWAY_URL;
 export const TOKEN_PATH = `${__dirname}/.hsm-refresh-token`;
