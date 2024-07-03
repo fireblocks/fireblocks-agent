f492ca3d163d133d2567ed3e174801f2647bab60
Nir Berman <nir@fireblocks.com>
2024-07-03 17:55:39 +0300

    Added keep alive from agent to customer server

--- src/customer-server-client.ts
+++ src/customer-server-client.ts
@@ -6,23 +6,24 @@ import messagesService from './services/messages.service';
 class CustomerClient {
   pullMessagesStatus = async () => {
     try {
-      const messages = messagesService.getPendingMessages();
-      if (!!messages.length) {
-        const requestsIds = messages.map((msg) => msg.messageStatus.requestId);
-        logger.info(`Pulling messages status for ${JSON.stringify(requestsIds)}`);
-        const { statuses: serverStatuses } = await customerServerApi.messagesStatus({ requestsIds });
-        if (!!serverStatuses.length) {
-          logger.info(`Got messages status for ${JSON.stringify(serverStatuses.map((status) => { return { requestId: status.requestId, status: status.status } }))}`);
+      const messages = messagesService.getPendingMessages() ?? [];
+      const requestsIds = messages.map((msg) => msg.messageStatus.requestId);
+      logger.info(`Pulling messages status for ${JSON.stringify(requestsIds)}`);
+      const { statuses: serverStatuses } = await customerServerApi.messagesStatus({ requestsIds });
+      if (!!serverStatuses.length) {
+        logger.info(`Got messages status for ${JSON.stringify(serverStatuses.map((status) => { return { requestId: status.requestId, status: status.status } }))}`);
 
-          await messagesService.updateStatus(serverStatuses.map((messagesStatus): ExtendedMessageStatusCache => {
-            const decodedMsg = messages.find((msg) => msg.messageStatus.requestId === messagesStatus.requestId);
-            return {
-              msgId: decodedMsg.msgId,
-              request: decodedMsg.request,
-              messageStatus: messagesStatus,
-            };
-          }));
-        }
+        await messagesService.updateStatus(serverStatuses.map((messagesStatus): ExtendedMessageStatusCache => {
+          const decodedMsg = messages.find((msg) => msg.messageStatus.requestId === messagesStatus.requestId);
+          return {
+            msgId: decodedMsg.msgId,
+            request: decodedMsg.request,
+            messageStatus: messagesStatus,
+          };
+        }));
+      }
+      else {
+        logger.info(`Customer server up - no messages status returned`);
       }
     } catch (e) {
       logger.error(`Got error from customer server ${e.message}`);
